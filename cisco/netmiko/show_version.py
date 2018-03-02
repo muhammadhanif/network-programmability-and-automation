bafc90c27b7252c93949eec0243dcdbdcbd723a6
Muhammad Hanif <moehammadhanif@gmail.com>
2018-03-02 09:30:38 +0700

    Add a file: show_version.py
    show_version.py in /cisco/netmiko

---
+++ cisco/netmiko/show_version.py
@@ -0,0 +1,23 @@
+"""
+Correct Me If I am Wrong
+
+MUHAMMAD HANIF
+Website  : http://hanifmu.com
+Email    : moehammadhanif[@]gmail.com
+Telegram : https://t.me/muhammad_hanif
+"""
+
+from netmiko import ConnectHandler
+
+device = {
+    'device_type': 'cisco_ios',
+    'ip': '10.10.1.1',
+    'username': 'hanif',
+    'password': 'hanif',
+    'secret':'hanif'
+}
+
+net_connect = ConnectHandler(**device)
+show_version = net_connect.send_command('show version')
+
+print(show_version)
