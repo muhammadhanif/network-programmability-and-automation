538e1e73d3b8a1c38272b0e94012be99fa1a81c9
Muhammad Hanif <moehammadhanif@gmail.com>
2018-04-24 09:00:11 +0700

    Update get_interfaces.py

--- cisco/napalm/get_interfaces.py
+++ cisco/napalm/get_interfaces.py
@@ -8,31 +8,57 @@ Telegram : https://t.me/muhammad_hanif
 """
 
 from napalm import get_network_driver
+from tabulate import tabulate
+import argparse
 
-driver = get_network_driver('ios')
-device = driver(hostname='10.10.1.1',
-                username='hanif',
-                password='hanif',
-                optional_args={'secret': 'hanif'})
-device.open()
+def run(host, username, password, enable):
+    driver = get_network_driver('ios')
+    device = driver(hostname = host,
+                    username = username,
+                    password = password,
+                    optional_args={'secret': enable})
 
-interfaces = device.get_interfaces()
+    try:
+        device.open()
 
-#print(interfaces)
+        interfaces = device.get_interfaces()
+        header = ['interface', 'is_enabled', 'is_up',
+                  'speed', 'last_flapped', 'mac_address', 'description']
+        table = [];
 
-print("\n")
-print("-" * 150)
-print("{:^30}{}{:^15}{}{:^10}{}{:^10}{}{:^15}{}{:^25}{}{:^20}".format('interface', '|' ,'is_enabled', '|', 'is_up', '|', 'speed', '|', 'last_flapped', '|', 'mac_address', '|', 'description'))
-print("-" * 150)
+        for interface, interface_info in interfaces.items():
+            is_enabled = 'True' if interface_info['is_enabled'] == 1 else 'False'
+            is_up= 'True' if interface_info['is_up'] == 1 else 'False'
 
-for interface, interface_info in interfaces.items():
-    is_enabled = 'True' if interface_info['is_enabled'] == 1 else 'False'
-    is_up= 'True' if interface_info['is_up'] == 1 else 'False'
+            table.append([interface, is_enabled,
+                          is_up,
+                          interface_info['speed'],
+                          interface_info['last_flapped'],
+                          interface_info['mac_address'],
+                          interface_info['description']])
 
-    print("{:^30}{}{:^15}{}{:^10}{}{:^10}{}{:^15}{}{:^25}{}{:^20}".format(interface, '|', is_enabled, '|',  is_up, '|', interface_info['speed'], '|', interface_info['last_flapped'], '|', interface_info['mac_address'], '|', interface_info['description']))
+        print(tabulate(table, headers = header, tablefmt='grid'))
 
-print("-" * 150)
+        device.close()
+    except:
+        print("We can't access {}. Check your credentials.".format(host))
 
-print('\n')
+if __name__ == '__main__':
+    description = "This script will give you interface information: \n \
+                   is_up  (True/False), is_enabled  (True/False),\
+                   speed (in Mbit), last_flapped (in seconds), \
+                   mac_address, and description."
 
-device.close()
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument("-ip", "--ip", required=True,
+                        help="ip address of cisco device")
+    parser.add_argument("-u", "--username", required=True,
+                        help="username for accessing cisco device")
+    parser.add_argument("-p", "--password", required=True,
+                        help="password for accessing cisco device")
+    parser.add_argument("-e", "--enable", required=True,
+                        help="enable password for accessing cisco device")
+
+    args = vars(parser.parse_args())
+
+    run(args['ip'], args['username'], args['password'], args['enable'])
